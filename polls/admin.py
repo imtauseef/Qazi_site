5d6cfa769f25b371ca5126b4b25e20fa47098b57
Tauseef Ahmad <tauseefkhan532@gmail.com>
2021-08-11 21:31:00 +0500

    changes in admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Question, Choices
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choices
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        ("question info", {'fields': ['question_text']}),
+        ("Date info", {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choices)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -10,6 +11,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description= 'published recently?'
+    )
 
     def was_published_recently(self):
         now = timezone.now()
